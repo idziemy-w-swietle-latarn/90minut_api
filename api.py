b97d6d1f2518c91c2aca232e8111b5a3e7d86362
idziemy_w_swietle_latarn <idziemy_w_swetle_latarn@protonmail.com>
2023-02-28 23:03:11 +0100

    def parse_player

---
+++ api.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_LINK = 'http://www.90minut.pl'
+KARIERA = '/kariera.php?id='
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def get_page_soup(link):
+    html_text = requests.get(link).text
+    return BeautifulSoup(html_text, 'html.parser')
+
+def search_results(search_phrase):
+    pass
+
+def get_first_finding(search_phrase):
+    link = search_results(search_phrase)[0].link
+    soup = get_page_soup(link)
+    
+def parse_player(id):
+    link = BASE_LINK + KARIERA + id
+    soup = get_page_soup(link)
+    player_dict = AttrDict()
+    player_dict.link = KARIERA + id
+    player_dict.name = soup.find('td', string="Imię").next_sibling.text
+    
+    return player_dict
--- api_test.py
+++ api_test.py
@@ -1,8 +1,12 @@
 from api import get_first_finding, parse_player
 from api import AttrDict
+import pytest
 
 
-cristian = AttrDict({
+
+@pytest.fixture
+def cristian_fxt():
+    cristian = AttrDict({
     'link': "/kariera.php?id=17454",
     'birthplace': ' San Miguel de Tucumán',
     'name': 'Cristián Omar (Cristián)',
@@ -13,9 +17,9 @@ cristian = AttrDict({
     'height' : '183',
     'first_club' : 'Club Gimnasia y Esgrima de Concepción del Uruguay'
 })
+    return cristian
 
-print(cristian.games)
-
+@pytest.mark.skip
 def test_get_first_finding():
     cristian = get_first_finding('Cristian Omar Diaz')
     assert cristian['link'] == "/kariera.php?id=17454"
@@ -31,3 +35,13 @@ def test_get_first_finding():
 
 def test_parse_player():
     cristian = parse_player('17454')
+    assert cristian['link'] == "/kariera.php?id=17454"
+    assert cristian['name'] == 'Cristián Omar (Cristián)'
+    assert cristian['birthplace'] == ' San Miguel de Tucumán'
+    assert cristian['position']
+    assert cristian.position == 'napastnik'
+    assert cristian.goals == '15'
+    assert cristian.games == '52'
+    assert cristian.birthdate == '3 listopada 1986'
+    assert cristian.height == '183'
+    assert cristian.first_club == 'Club Gimnasia y Esgrima de Concepción del Uruguay'
